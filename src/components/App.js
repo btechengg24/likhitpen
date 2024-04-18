57340e7195092e3492ac9a4a19f7785776369d53
btechengg24 <btechengg24@gmail.com>
2024-04-18 20:02:46 +0530

    code modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import Editor from './Editor'
-import useLocalStorage from '../hooks/useLocalStorage'
+import React, { useState, useEffect } from "react";
+import Editor from "./Editor";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Navbar from "./Navbar";
 
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
-  const [srcDoc, setSrcDoc] = useState('')
+	const [html, setHtml] = useState("");
+	const [css, setCss] = useState("");
+	const [js, setJs] = useState("");
+	const [srcDoc, setSrcDoc] = useState("");
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setSrcDoc(`
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setSrcDoc(`
         <html>
           <body>${html}</body>
           <style>${css}</style>
           <script>${js}</script>
         </html>
-      `)
-    }, 250)
+      `);
+		}, 250);
 
-    return () => clearTimeout(timeout)
-  }, [html, css, js])
+		return () => clearTimeout(timeout);
+	}, [html, css, js]);
 
-  return (
-    <>
-      <div className="pane top-pane">
-        <Editor
-          language="xml"
-          displayName="HTML"
-          value={html}
-          onChange={setHtml}
-        />
-        <Editor
-          language="css"
-          displayName="CSS"
-          value={css}
-          onChange={setCss}
-        />
-        <Editor
-          language="javascript"
-          displayName="JS"
-          value={js}
-          onChange={setJs}
-        />
-      </div>
-      <div className="pane">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-        />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Navbar />
+			</div>
+			<div className="pane top-pane">
+				<Editor
+					language="xml"
+					displayName="HTML"
+					value={html}
+					onChange={setHtml}
+				/>
+				<Editor
+					language="css"
+					displayName="CSS"
+					value={css}
+					onChange={setCss}
+				/>
+				<Editor
+					language="javascript"
+					displayName="JS"
+					value={js}
+					onChange={setJs}
+				/>
+			</div>
+			<div className="pane">
+				<iframe
+					srcDoc={srcDoc}
+					title="output"
+					sandbox="allow-scripts"
+					frameBorder="0"
+					width="100%"
+					height="100%"
+				/>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import icon from ".././assets/codepen_icon.png";
+// import icon from ".././assets/webicon.png"
+const Navbar = () => {
+	return (
+		<div className="wrapper">
+			<div className="navbar">
+				<div className="navbar-title" fle>
+					LikhitPen
+				</div>
+			</div>
+			{/* <div>
+				<img src={icon} alt="icon" width={50} height={50} loading="lazy" />
+			</div> */}
+		</div>
+	);
+};
+
+export default Navbar;
